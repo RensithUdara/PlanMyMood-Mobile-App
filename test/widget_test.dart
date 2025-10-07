82959415bbf0390a36542d802752422aad5f3776
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-10-07 14:53:38 +0530

    Refactor widget test to verify splash screen and remove counter logic for clarity

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,17 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:planmymood_mobileapp/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('PlanMyMood app smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PlanMyMoodApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that splash screen appears
+    expect(find.text('PlanMyMood!'), findsOneWidget);
+    expect(find.text('Your personal mood-based task planner'), findsOneWidget);
   });
 }
