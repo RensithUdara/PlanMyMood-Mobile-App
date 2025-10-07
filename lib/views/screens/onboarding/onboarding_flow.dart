946f0552da45b01fb88fed5d6401f09a78468a32
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-10-07 14:53:04 +0530

    Remove unused imports in OnboardingFlow widget for cleaner code

--- lib/views/screens/onboarding/onboarding_flow.dart
+++ lib/views/screens/onboarding/onboarding_flow.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../../../controllers/app_controller.dart';
 import '../../../utils/app_colors.dart';
 import '../../../utils/app_constants.dart';
 import '../../../views/widgets/onboarding_page.dart';
