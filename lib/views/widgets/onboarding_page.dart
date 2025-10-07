ad4f0bb97497dac6c20abed5ce5218ed74a9d4b3
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-10-07 14:49:43 +0530

    Refactor OnboardingPage widget for improved code formatting and readability

--- lib/views/widgets/onboarding_page.dart
+++ lib/views/widgets/onboarding_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../../utils/app_colors.dart';
 import '../../utils/app_constants.dart';
 import '../screens/onboarding/onboarding_flow.dart';
@@ -15,12 +16,13 @@ class OnboardingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: data.backgroundColor,
-      padding: const EdgeInsets.symmetric(horizontal: AppConstants.paddingLarge),
+      padding:
+          const EdgeInsets.symmetric(horizontal: AppConstants.paddingLarge),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Spacer(),
-          
+
           // Illustration (using emoji as placeholder)
           Container(
             width: 200,
@@ -28,11 +30,11 @@ class OnboardingPage extends StatelessWidget {
             decoration: BoxDecoration(
               color: AppColors.lightCream,
               borderRadius: BorderRadius.circular(20),
-              boxShadow: [
+              boxShadow: const [
                 BoxShadow(
                   color: AppColors.shadow,
                   blurRadius: 20,
-                  offset: const Offset(0, 10),
+                  offset: Offset(0, 10),
                 ),
               ],
             ),
@@ -43,32 +45,32 @@ class OnboardingPage extends StatelessWidget {
               ),
             ),
           ),
-          
+
           const SizedBox(height: AppConstants.paddingXLarge),
-          
+
           // Title
           Text(
             data.title,
             style: Theme.of(context).textTheme.displayMedium?.copyWith(
-              color: AppColors.primaryText,
-              fontWeight: FontWeight.bold,
-            ),
+                  color: AppColors.primaryText,
+                  fontWeight: FontWeight.bold,
+                ),
             textAlign: TextAlign.center,
           ),
-          
+
           const SizedBox(height: AppConstants.paddingMedium),
-          
+
           // Subtitle
           if (data.subtitle.isNotEmpty)
             Text(
               data.subtitle,
               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                color: AppColors.secondaryText,
-                fontWeight: FontWeight.normal,
-              ),
+                    color: AppColors.secondaryText,
+                    fontWeight: FontWeight.normal,
+                  ),
               textAlign: TextAlign.center,
             ),
-          
+
           const Spacer(flex: 2),
         ],
       ),
