7d132deec06e77a0ce1ea0f6e126b1600ad70b03
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-10-07 14:53:46 +0530

    Remove unnecessary blank line in widget test for cleaner code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:planmymood_mobileapp/main.dart';
 
 void main() {
